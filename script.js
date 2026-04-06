aee0f858acd1754938b31667bb0bedcb75b0c4b6
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-04-06 16:33:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", (event) => {
-  const newP = document.createElement("p");
-
-  const newContent = document.createTextNode("DOM load success");
-
-  newP.appendChild(newContent);
-
-  const currentDiv = document.getElementById("para");
-  document.body.insertBefore(newP, currentDiv);
+    document.body.textContent = "DOM load success";
 });
